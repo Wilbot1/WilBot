e915c12812b346a37dedaffc74d8e0a5824138e2
Wilbot1 <101874174+Wilbot1@users.noreply.github.com>
2022-03-18 22:27:24 -0500

    Update Wil.js

--- Wil.js
+++ Wil.js
@@ -9,7 +9,7 @@ async function iniciar () {
         client.on('qr', () => {
         })
 
-        fs.existsSync('./Wil.json') && client.loadAuthInfo('./Wil.json')
+        fs.existsSync('./Samu330.json') && client.loadAuthInfo('./Samu330.json')
 
         client.on('connecting', () => {
         console.log('Conectando')
@@ -19,7 +19,7 @@ async function iniciar () {
         console.log('Conectado exitosamente :D')
         })
         await client.connect({timeoutMs: 30*1000})
-        fs.writeFileSync('./Wil.json', JSON.stringify(client.base64EncodedAuthInfo(), null, '\t'))
+        fs.writeFileSync('./Samu330.json', JSON.stringify(client.base64EncodedAuthInfo(), null, '\t'))
         }
 
 iniciar ()
